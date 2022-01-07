32e19eed60658cd79c1fe585924a6760c57df346
Abhista Partal <s210246@student.dtu.dk>
2022-01-07 16:20:51 +0100

    Basic changes to train.py file

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -2,11 +2,12 @@ import torch
 from torch import nn, optim
 import torch.nn.functional as F
 from tqdm.notebook import tqdm_notebook
+from dataset_fetcher import Dataset_fetcher
 
 class Classifier(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(1080*720, 3)
+        self.fc1 = nn.Linear(512*512, 3)
     
     def forward(self, x):
         x = x.view(x.shape[0], -1)
@@ -19,13 +20,14 @@ class Classifier(nn.Module):
 model = Classifier()
 optimizer = optim.Adam(model.parameters(), lr = 0.003)
 criterion = nn.NLLLoss()
-#data = datavariable
+DS = Dataset_fetcher('../../data/raw/COVID19_Pneumonia_Normal_Chest_Xray_PA_Dataset')
+loader = torch.utils.data.DataLoader(DS, shuffle=False, num_workers=0, batch_size=3)
 
 epochs = 10
 
 for e in tqdm_notebook(range(epochs), desc = f"Epochs"):
     running_loss = 0
-    for images, labels in tqdm_notebook(data, desc = f"Batch number: {e + 1}"):
+    for images, labels in tqdm_notebook(loader, desc = f"Batch number: {e + 1}"):
         model.train()
         optimizer.zero_grad()
         output = model(images)
