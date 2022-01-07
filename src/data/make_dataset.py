e6ad4bb20e40feb869c6e666f10e5e409a09ba22
slangen <steven_steven@hotmail.de>
2022-01-07 14:45:41 +0100

    downlaod zip into raw and unzip

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,18 +1,59 @@
 # -*- coding: utf-8 -*-
-import click
+import argparse
+import sys
+
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
+import requests, zipfile, io
+import os
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+
+url="https://data.mendeley.com/public-files/datasets/jctsfj2sfn/files/148dd4e7-636b-404b-8a3c-6938158bc2c0/file_downloaded"
+
+def download_extract(zip_file_url,PATH,filename="covid19-pneumonia-normal-chest-xraypa-dataset.zip",
+foldername='COVID19_Pneumonia_Normal_Chest_Xray_PA_Dataset'):
+
+    if PATH[-1] != '/':
+        PATH.append('/')
+    download=True
+    extract=True
+    if os.path.isfile(PATH+filename):
+        download=False
+    if os.path.isdir(PATH+foldername):
+        extract=False
+
+    if download:
+        r = requests.get(zip_file_url)
+        if extract:
+            z = zipfile.ZipFile(io.BytesIO(r.content))
+            z.extractall(PATH)
+    elif extract:
+        z=zipfile.ZipFile(PATH+name)
+        z.extractall(PATH)
+
+
+def main():
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
+    parser = argparse.ArgumentParser(description='Data dwonloading and unzipping arguments')
+    parser.add_argument("--url",type=str, default="https://data.mendeley.com/public-files/datasets/jctsfj2sfn/files/148dd4e7-636b-404b-8a3c-6938158bc2c0/file_downloaded",help="data URL to zip file")
+    parser.add_argument("--PATH",type=str, default='../../data/raw/',help="where to save zip")
+    parser.add_argument("--NAME", type=str, default="covid19-pneumonia-normal-chest-xraypa-dataset.zip", help="name of file to be extracted")
+    parser.add_argument("--exdir", type=str, default='COVID19_Pneumonia_Normal_Chest_Xray_PA_Dataset', help="name of dir to be extracted")
+
+    args = parser.parse_args(sys.argv[2:])
+    zip_file_url=args.url
+    PATH=args.PATH
+    filename=args.NAME
+    foldername=args.exdir
+    download_extract(zip_file_url,PATH,filename,foldername)
+
     logger = logging.getLogger(__name__)
+
+    
     logger.info('making final data set from raw data')
 
 
