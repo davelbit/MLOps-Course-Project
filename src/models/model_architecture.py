fd84c9627bb7043e9de001ecde366db1e66a0ae6
RianLeevinson <rianleevinson@yahoo.com>
2022-01-14 12:51:41 +0100

    model_architecture_precommit

--- src/models/model_architecture.py
+++ src/models/model_architecture.py
@@ -18,18 +18,21 @@ import torch.nn.functional as F
 
 
 class XrayClassifier(nn.Module):
-    """Class to define the model architecture"""
+    """
+    Class to define the model architecture
+    Ensure input image size is 32x32
+    """
 
     def __init__(self):
         super(XrayClassifier, self).__init__()
 
         self.conv1 = nn.Conv2d(1, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120) 
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
-    def forward(self, x : torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         """forward loop for the model"""
 
         x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
@@ -38,5 +41,5 @@ class XrayClassifier(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
-        
+
         return x
