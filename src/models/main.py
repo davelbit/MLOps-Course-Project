6b5236a0a5d56949a3fabc8bff3a7c22fd8026c9
slangen <steven_steven@hotmail.de>
2022-01-14 14:25:04 +0100

    woeking on cloud

--- src/models/main.py
+++ src/models/main.py
@@ -3,22 +3,3 @@ from train_model import Training_loop
 if __name__ == "__main__":
     trainer = Training_loop()
     trainer.loop()
-
-
-"""
-gcloud ai-platform jobs submit training ${JOB_NAME} \
-  --region=us-central1 \
-  --master-image-uri=gcr.io/cloud-ml-public/training/pytorch-xla.1-10 \
-  --scale-tier=BASIC \
-  --job-dir=${JOB_DIR} \
-  --package-path=./trainer \
-  --module-name=trainer.task \
-  -- \
-  --train-files=gs://cloud-samples-data/ai-platform/chicago_taxi/training/small/taxi_trips_train.csv \
-  --eval-files=gs://cloud-samples-data/ai-platform/chicago_taxi/training/small/taxi_trips_eval.csv \
-  --num-epochs=10 \
-  --batch-size=100 \
-  --learning-rate=0.001
-
-
-"""
