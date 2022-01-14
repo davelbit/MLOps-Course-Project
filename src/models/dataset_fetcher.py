e81d58e9bd759f32023db5a48c336215db38d6f5
davelbit <s202385@student.dtu.dk>
2022-01-14 14:24:17 +0100

    Add type annotation to data_fetcher

--- src/models/dataset_fetcher.py
+++ src/models/dataset_fetcher.py
@@ -1,31 +1,50 @@
+#!/usr/bin/env python3
+######################################################################
+# Authors:      <s202540> Rian Leevinson
+#                     <s202385> David Parham
+#                     <s193647> Stefan Nahstoll
+#                     <s210246> Abhista Partal Balasubramaniam
+#
+# Course:        Machine Learning Operations
+# Semester:    Spring 2022
+# Institution:  Technical University of Denmark (DTU)
+#
+# Module: This module is responsible accessing our data
+######################################################################
+
+from typing import Union
+
 import torch
-
-# from torch.jit import Error
-from torch.utils.data import Dataset
-
-# import numpy as np
-
-# import torchvision
-
-# import os
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, Dataset
 
 
+# TODO: Write tests for this module
 class Dataset_fetcher(Dataset):
-    def __init__(self, PATH_IMG, transform=None):
-        self.transform = transform
+    def __init__(
+        self, PATH_IMG: str, transform: Union[transforms.transforms.Compose, None] = None
+    ) -> None:
 
+        print(type(transform))
+        self.transform = transform
         self.images = torch.load(PATH_IMG)
-        print(self.images)
         # self.labels = torch.load(Path_LAB)
 
-    def __getitem__(self, idx):
-        x = self.images[idx]
-        y = self.labels[idx]
+    def __getitem__(self, idx: int) -> Union[torch.tensor, str]:
+        image = self.images[idx]
+        label = self.labels[idx]
 
         if self.transform:
-            x = self.transform(x)
+            image = self.transform(image)
 
-        return x, y
+        return image, label
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.images)
+
+
+if __name__ == "__main__":
+    path = "data/raw/COVID19_Pneumonia_Normal_Chest_Xray_PA_Dataset"
+    dataset = Dataset_fetcher(path)
+    dataloader = DataLoader(dataset, shuffle=False, num_workers=4, batch_size=3)
+    image, label = next(iter(dataloader))
