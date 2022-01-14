3276b5e7a0adb24b58e1f3738ac100777934590b
davelbit <s202385@student.dtu.dk>
2022-01-14 11:56:48 +0100

    Current version

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,26 +1,29 @@
 import torch
-from torch import nn, optim
 import torch.nn.functional as F
-from tqdm.notebook import tqdm_notebook
 from dataset_fetcher import Dataset_fetcher
 from model_architecture import XrayClassifier
+from torch import nn, optim
+from tqdm.notebook import tqdm_notebook
 
-class Training_loop():
-    def __init__(self, path = 'data/raw/COVID19_Pneumonia_Normal_Chest_Xray_PA_Dataset'):
-        
+
+class Training_loop:
+    def __init__(self, path="data/raw/COVID19_Pneumonia_Normal_Chest_Xray_PA_Dataset"):
+
         self.path = path
         self.model = XrayClassifier(3)
-        self.optimizer = optim.Adam(self.model.parameters(), lr = 0.003)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=0.003)
         self.criterion = nn.NLLLoss()
         self.DS = Dataset_fetcher(self.path)
-        self.loader = torch.utils.data.DataLoader(self.DS, shuffle=False, num_workers=0, batch_size=3)
+        self.loader = torch.utils.data.DataLoader(
+            self.DS, shuffle=False, num_workers=0, batch_size=3
+        )
         self.epochs = 10
 
     def loop(self):
 
-        for e in tqdm_notebook(range(self.epochs), desc = f"Epochs"):
+        for e in tqdm_notebook(range(self.epochs), desc="Epochs"):
             running_loss = 0
-            for images, labels in tqdm_notebook(self.loader, desc = f"Batch number: {e + 1}"):
+            for images, labels in tqdm_notebook(self.loader, desc=f"Batch number: {e + 1}"):
                 # false if image is not readable
                 if images is not False:
                     self.model.train()
@@ -30,11 +33,11 @@ class Training_loop():
                     self.loss.backward()
                     self.optimizer.step()
                     running_loss += self.loss.item()
-            else:
-                if images is not False:
-                    with torch.no_grad():
-                        self.model.eval()
-                        top_p, top_class = torch.exp(self.model(images)).topk(1, dim = 1)
-                        equals = top_class == labels.view(*top_class.shape)
-                        self.accuracy = torch.mean(equals.type(torch.FloatTensor))
-                    print(f"Accuracy: {self.accuracy.item()*100}%")
+
+            if images is not False:
+                with torch.no_grad():
+                    self.model.eval()
+                    top_p, top_class = torch.exp(self.model(images)).topk(1, dim=1)
+                    equals = top_class == labels.view(*top_class.shape)
+                    self.accuracy = torch.mean(equals.type(torch.FloatTensor))
+                print(f"Accuracy: {self.accuracy.item()*100}%")
