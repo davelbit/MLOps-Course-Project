d0777de2900af062b169352d9f5a3c393759cdd2
davelbit <s202385@student.dtu.dk>
2022-01-15 17:14:32 +0100

    Finalized inference. Script includes total and class accuracies, plots and wandb

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -12,67 +12,149 @@
 # Module: This module is responsible for prediction
 ######################################################################
 
-import os
+from typing import List
 
-import hydra
-import matplotlib.pyplot as plt
 import numpy as np
-import omegaconf
 import torch
+import wandb
 from dataset_fetcher import Dataset_fetcher
-from model_architecture import XrayClassifier
+from matplotlib import pyplot as plt
+from omegaconf import OmegaConf
 from torch import nn
+from tqdm import tqdm
 
 
 def get_model_from_checkpoint(path: str) -> nn.Module:
     """Returns a loaded model from checkpoint"""
 
-    from src.models.model_architecture import XrayClassifier
+    from model_architecture import XrayClassifier
 
     model = XrayClassifier()
-    model.load_state_dict(torch.load(path))
-    model.eval()
+    checkpoint = torch.load(path)
+    model.load_state_dict(checkpoint["model_state_dict"])
     return model
 
 
-@hydra.main(config_name="config.yaml")
-def make_predictions(
-    config: omegaconf.dictconfig.DictConfig, model: nn.Module = None, load_model: bool = False
-) -> None:
+def create_plot(data: List[torch.tensor], classes: tuple) -> plt:
+    """Create plots"""
+
+    # initialize a figure
+    fig = plt.figure(figsize=(8, 8))
+
+    # loop over the stored images
+    for i, (image, label, prediction) in enumerate(data):
+
+        # create a subplot
+        ax = plt.subplot(2, 2, i + 1)
+
+        # grab the image, convert it from channels first ordering to
+        # channels last ordering, and scale the raw pixel intensities
+        # to te range [0, 255]
+        image = image[0].numpy()
+        image = (image * 255.0).astype("uint8")
+        # show the image along with the label
+        plt.imshow(np.transpose(image, (1, 2, 0)))
+        plt.title(f"Groundtruth: {classes[label]}", fontsize=14)
+        plt.ylabel(f"Predicted: {classes[prediction]}", fontsize=14)
+        plt.xticks([])
+        plt.yticks([])
+
+    return plt
+
+
+def inference(VAL_PATHS: dict[str, str], model: nn.Module = None, load_model: bool = False) -> None:
     """Classify unseen images from a validation set the model hasn't seen in training or testing"""
 
-    # Load dataset
-    # dataset = torch.load(config.VALIDATIONSET)
-    dataset = Dataset_fetcher(path)
-    dataloader = torch.utils.data.DataLoader(dataset, shuffle=False, num_workers=0, batch_size=3)
+    # set flags / seeds
+    np.random.seed(1)
+    torch.manual_seed(1)
+
+    # Load config file
+    config = OmegaConf.load("config.yaml")
+
+    # Initialize logging with wandb and track conf settings
+    wandb.init(project="MLOps-Project", config=dict(config))
+
+    # Optimizer Hyperparameter / const variables
+    BATCH_SIZE = 1
+    N_WORKERS = config.N_WORKERS
+
+    print("[INFO] Load dataset from disk...")
+    validation_set = Dataset_fetcher(VAL_PATHS["images"], VAL_PATHS["labels"])
+
+    print("[INFO] Prepare dataloader...")
+    validationloader = torch.utils.data.DataLoader(
+        validation_set, shuffle=False, num_workers=N_WORKERS, batch_size=BATCH_SIZE
+    )
+
+    classes = ("covid", "normal", "pneumonia")
+
+    if load_model:
+        # Loading saved model
+        model = get_model_from_checkpoint(config.BEST_MODEL_PATH)
+
+    wandb.watch(model, log_freq=100)
 
     # Disable gradient tracking
     with torch.no_grad():
-        # Retrieve item
-        index = 256
-        item = dataset[index]
-        image = item[0]
-        true_target = item[1]
+        model.eval()
 
-        if load_model:
-            # Loading the saved model
-            model = get_model_from_checkpoint(config.MODEL_PATH)
+        correct = 0
+        total = 0
 
-        model = XrayClassifier()
-        # Generate prediction
-        prediction = model(image)
+        stored_images = []
 
-        # Predicted class value using argmax
-        predicted_class = np.argmax(prediction)
+        # prepare to count predictions for each class
+        correct_pred = {classname: 0 for classname in classes}
+        total_pred = {classname: 0 for classname in classes}
 
-        # Reshape image
-        image = image.reshape(28, 28, 1)
+        with tqdm(validationloader) as progress_bar:
+            for i, (images, labels) in enumerate(progress_bar, 1):
 
-        # Show result
-        plt.imshow(image, cmap="gray")
-        plt.title(f"Prediction: {predicted_class} - Actual target: {true_target}")
-        plt.show()
+                progress_bar.set_description("[INFO] Running inference...")
+
+                # Generate prediction
+                output = model(images)
+
+                # Predicted class value
+                _, predicted = torch.max(output.data, 1)
+
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+                stored_images.append((images, labels, predicted))
+
+                if i % 4 == 0:
+                    plot = create_plot(stored_images, classes)
+                    wandb.log({"examples": wandb.Image(plot)})
+                    plot.close()
+                    stored_images = []
+
+                # collect the correct predictions for each class
+                for label, prediction in zip(labels, predicted):
+                    if label == prediction:
+                        correct_pred[classes[label]] += 1
+                    total_pred[classes[label]] += 1
+
+    print("[INFO] Calculating model performance...")
+    for classname, correct_count in correct_pred.items():
+        accuracy = 100 * float(correct_count) / total_pred[classname]
+        print(f"Accuracy for classes:  {classname} is {accuracy:.1f} %")
+
+    print(
+        f"\nAccuracy of the network on the {total} validation images: {100 * correct // total} %\n"
+    )
 
 
 if __name__ == "__main__":
-    make_predictions()
+
+    # this path must be adapted to your own machine
+    root_dir = "/home/davidparham/Workspaces/DTU/MLOps/project/"
+
+    # TODO: Validation files need to be created
+    VAL_PATHS = {
+        "images": root_dir + "data/preprocessed/covid_not_norm/test_images.pt",
+        "labels": root_dir + "data/preprocessed/covid_not_norm/test_labels.pt",
+    }
+
+    inference(VAL_PATHS, load_model=True)
